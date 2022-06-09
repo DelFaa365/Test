eb8428ee2eb2de074dc3e2dba31eb3b33c265362
DelFaa365 <Tomasinka123@gmail.com>
2022-06-09 11:12:58 +0200

    update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,61 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        char [][] znaky = {
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'+', '-', '=', '/', '_', '.', ',', ' '},
+            {'A', 'B', 'C', 'a', 'b', 'c', ' ', ' '},
+            {'D', 'E', 'F', 'd', 'e', 'f', ' ', ' '},
+            {'G', 'H', 'I', 'g', 'h', 'i', ' ', ' '},
+            {'J', 'K', 'L', 'j', 'k', 'l', ' ', ' '},
+            {'M', 'N', 'O', 'm', 'n', 'o', ' ', ' '},
+            {'P', 'Q', 'R', 'S', 'p', 'q', 'r', 's'},
+            {'T', 'U', 'V', 't', 'u', 'v', ',', ' '},
+            {'W', 'X', 'Y', 'Z', 'w', 'x', 'y', 'z'},
+        };
+
+        System.out.println("Zadej jméno");
+        String veta = sc.nextLine();
+        char [] slovicka = veta.toCharArray();
+        String vysledek = "";
+        int kolikPricist= 0;
+        int chybneZnaky= slovicka.length;
+        if(slovicka.length < 9){
+            kolikPricist = 9-slovicka.length;
+        }
+
+        for (int i = 0; i < slovicka.length; i++) {
+            for (int j = 0; j < znaky.length; j++) {
+                for (int j2 = 0; j2 < 8; j2++) {
+                    if(slovicka[i] == znaky[j][j2]){
+                        vysledek+=j;
+                        if(i==2 || i==5){
+                            vysledek+=" ";
+                        }
+                        chybneZnaky--;
+                    }
+                }
+            }
+        }
+
+        if(chybneZnaky != 0){
+            System.out.println("Zadal jsi chybné znaky");
+            return;
+        }
+
+        System.out.print(vysledek);
+
+        while(kolikPricist > 0){
+            System.out.print("0");
+            kolikPricist--;
+        }
+
+
+
+
+
     }
 }
